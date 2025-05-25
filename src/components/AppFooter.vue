bf550e378543029875e0299e757f90a2a21a8380
georgi <Georgi_Ivanov92@pm.me>
2025-05-25 16:48:36 +0200

    add Vuetify to the project

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>I am Footer</h2>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
-import './assets/main.css'
+import './styles/reset.css'
+import '@mdi/font/css/materialdesignicons.css'
 
+import 'vuetify/styles'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
