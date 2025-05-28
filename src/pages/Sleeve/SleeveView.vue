8ce4b0d3ff48c64e6d564f5d49228b98646a993c
georgi <Georgi_Ivanov92@pm.me>
2025-05-28 16:17:04 +0200

    add Axios for Fetch requests

--- src/pages/Sleeve/SleeveView.vue
+++ src/pages/Sleeve/SleeveView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import SleeveTable from './components/SleeveTable.vue';
 
+const route = useRoute();
+
+const queryValue = route.query.sleeveSequence;
+const sequenceNumber: number = Number(queryValue);
 </script>
 
 <template>
   <h1>Sleeve View Works</h1>
-  <SleeveTable />
+  <h2>{{ queryValue }}</h2>
+  <SleeveTable :sequenceNumber="sequenceNumber" />
 </template>
--- src/pages/Sleeve/components/SleeveTable.vue
+++ src/pages/Sleeve/components/SleeveTable.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { SleeveControllerApi } from '@/openapi';
+import { tr } from 'vuetify/locale';
+
+const props = defineProps<{
+  sequenceNumber?: number;
+}>();
+
+const number = Number(props.sequenceNumber);
+
+const sleeveApi = new SleeveControllerApi();
+const testNumber: number = 1002;
+async function getSleevesBySequenceNumber() {
+  console.log("Clicked");
+  try {
+    const response = await sleeveApi.getSleeveSequenceNumber(testNumber);
+    console.log('res', response.data);
+    return response.data;
+  } catch (e) {
+    console.error('Opps unexpected', e);
+    return [];
+  }
+}
+
+</script>
+
 <template>
   <h1>Sleeve Table Works</h1>
+  <p>{{ sequenceNumber }}</p>
+  <h1>{{ sequenceNumber }}</h1>
+  <v-btn @click="getSleevesBySequenceNumber" prepend-icon="$vuetify" variant="tonal">
+    Button
+  </v-btn>
 </template>
