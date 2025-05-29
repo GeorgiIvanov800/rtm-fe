69796ce92200ea0cfa350415edfa253235e51913
georgi <Georgi_Ivanov92@pm.me>
2025-05-29 19:07:02 +0200

    created util function to format date

--- src/pages/Sleeve/components/SleeveTable.vue
+++ src/pages/Sleeve/components/SleeveTable.vue
@@ -1,26 +1,51 @@
 <script setup lang="ts">
-import { SleeveControllerApi } from '@/openapi';
+import { SleeveControllerApi, type SleeveResponse } from '@/openapi';
+import { computed, ref } from 'vue';
+import { type DataTableHeader } from 'vuetify';
 import { tr } from 'vuetify/locale';
 
 const props = defineProps<{
   sequenceNumber?: number;
 }>();
 
+const sleeveData = ref(<SleeveResponse[]>([]));
+const EMPTY_MESSAGE = ref('Keine Sleeves mit der angegebenen Satznummer gefunden. Bitte versuchen Sie es mit einer anderen Nummer.');
 const number = Number(props.sequenceNumber);
 
 const sleeveApi = new SleeveControllerApi();
 const testNumber: number = 1002;
+
+
+
 async function getSleevesBySequenceNumber() {
   console.log("Clicked");
   try {
     const response = await sleeveApi.getSleeveSequenceNumber(testNumber);
-    console.log('res', response.data);
-    return response.data;
+    return sleeveData.value = response.data;
   } catch (e) {
     console.error('Opps unexpected', e);
     return [];
   }
 }
+
+const headers: DataTableHeader[] = [
+  { title: 'Satz Nummer', key: 'sequenceNumber', align: 'start', sortable: true },
+  { title: 'Sleeve Number', key: 'sleeveNumber', align: 'start', sortable: true },
+  { title: 'Type', key: 'type', align: 'start', sortable: true },
+  { title: 'Design', key: 'design', align: 'start', sortable: true },
+  { title: 'Farbe', key: 'color', align: 'start', sortable: true },
+  { title: 'Lager', key: 'warehouse', align: 'start', sortable: true },
+  { title: 'Platz', key: 'slot', align: 'start', sortable: true },
+  { title: 'Datum', key: 'manufactureDate', align: 'start', sortable: true },
+  { title: 'Zahnrad', key: 'gear', align: 'start', sortable: true },
+  { title: 'Anmerkungen', key: 'notes', align: 'start', sortable: true },
+  { title: 'Width', key: 'width', align: 'start', sortable: true },
+  { title: 'Km Stand', key: 'kmStand', align: 'start', sortable: true },
+  { title: 'Status', key: 'status', align: 'start', sortable: true },
+  { title: 'Zustand', key: 'condition', align: 'start', sortable: true },
+  { title: 'Hersteller', key: 'manufacturer', align: 'start', sortable: true },
+
+];
 
 </script>
 
@@ -31,4 +56,41 @@ async function getSleevesBySequenceNumber() {
   <v-btn @click="getSleevesBySequenceNumber" prepend-icon="$vuetify" variant="tonal">
     Button
   </v-btn>
+  <v-container>
+    <v-data-table-virtual :headers="headers" :items="sleeveData" item-value="sequenceNumber"
+      :no-data-text="EMPTY_MESSAGE" hide-default-footer show-expand>
+      <template v-slot:item.warehouse="{ item }">
+        {{ item.warehouse?.name }}
+      </template>
+      <template #[`item.data-table-expand`]="{ internalItem, isExpanded, toggleExpand }">
+        <v-btn :append-icon="isExpanded(internalItem) ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          :text="isExpanded(internalItem) ? 'Collapse' : 'More info'" class="text-none" color="medium-emphasis"
+          size="small" variant="text" border slim @click="toggleExpand(internalItem)"></v-btn>
+      </template>
+
+      <template v-slot:expanded-row="{ columns, item }">
+
+        <tr>
+          <td :colspan="columns.length" class="py-2">
+            <v-sheet rounded="lg" border>
+              <v-table density="compact">
+                <tbody class="bg-surface-light">
+                  <tr>
+                    <th>Lager</th>
+                  </tr>
+                </tbody>
+
+                <tbody>
+                  <tr>
+                    <td>{{ item.warehouse?.name }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-sheet>
+          </td>
+        </tr>
+      </template>
+    </v-data-table-virtual>
+  </v-container>
+
 </template>
---
+++ src/utils/formatDate.ts
@@ -0,0 +1,16 @@
+export function formatDate(
+  isoDateString: string,
+  locale = navigator.language,
+  options?: Intl.DateTimeFormatOptions,
+): string {
+  // Parse the “YYYY-MM-DD” string into a Date
+  const [year, month, day] = isoDateString.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
+
+  return new Intl.DateTimeFormat(locale, {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    ...options,
+  }).format(date);
+}
