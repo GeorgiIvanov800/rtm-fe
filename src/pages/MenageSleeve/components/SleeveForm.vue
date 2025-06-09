49cd3782f13575a687f3f2c1f3c65c3a091a8750
georgi <Georgi_Ivanov92@pm.me>
2025-06-09 13:45:07 +0200

    add validations

--- src/pages/MenageSleeve/components/SleeveForm.vue
+++ src/pages/MenageSleeve/components/SleeveForm.vue
@@ -61,6 +61,17 @@ const warehouseOptions: string[] = ['G', 'L1', 'L3'];
 const { handleSubmit } = useForm<{
   sequenceNumber: number;
   sleeveNumber: number;
+  design: string;
+  color: string;
+  manufacturer: string;
+  gear: number;
+  circumference: number;
+  slot: number;
+  width: number;
+  warehouse: string;
+  type: string;
+  condition: string;
+  manufactureDate: Date | null;
 
 }>({
   validationSchema,
@@ -69,10 +80,23 @@ const { handleSubmit } = useForm<{
 
 const sequenceNumber = useField<number>('sequenceNumber');
 const sleeveNumber = useField<number>('sleeveNumber');
+const design = useField<string>('design');
+const color = useField<string>('color');
+const manufacturer = useField<string>('manufacturer');
+const gear = useField<number>('gear');
+const circumference = useField<number>('circumference');
+const slot = useField<number>('slot');
+const width = useField<number>('width');
+const warehouse = useField<string>('warehouse');
+const type = useField<string>('type');
+const condition = useField<string>('condition');
+const { value: manufactureDate } =
+  useField<Date | null>('manufactureDate');
 
 
 const onSubmit = handleSubmit((values) => {
   console.log('On Submit clicked');
+  console.log(manufactureDate.value);
   alert(JSON.stringify(values, null, 2));
 });
 
@@ -103,42 +127,51 @@ function cancel() {
                     :error-messages="sleeveNumber.errorMessage.value ? [sleeveNumber.errorMessage.value] : []" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field v-model="formData.color" label="Farbe" outlined dense />
+                  <v-text-field v-model="color.value.value" label="Farbe" outlined dense
+                    :error-messages="color.errorMessage.value" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field v-model="formData.design" label="Motiv" outlined dense />
+                  <v-text-field v-model="design.value.value" label="Motiv" outlined dense
+                    :error-messages="design.errorMessage.value" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-select v-model="formData.type" label="Sleeve typ" outlined dense :items="typesOptions"
-                    item-title="label" item-value="value" />
+                  <v-select v-model="type.value.value" label="Sleeve typ" outlined dense :items="typesOptions"
+                    item-title="label" item-value="value" :error-messages="type.errorMessage.value" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field v-model="formData.manufacturer" label="Hersteller" outlined dense />
+                  <v-text-field v-model="manufacturer.value.value" label="Hersteller" outlined dense
+                    :error-messages="manufacturer.errorMessage.value" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-date-input clearable label="Herstellungsdatum"></v-date-input>
+                  <v-date-input v-model="manufactureDate" clearable label="Herstellungsdatum"></v-date-input>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-number-input v-model="formData.gear" label="Zahnrad"></v-number-input>
+                  <v-number-input v-model="gear.value.value" label="Zahnrad"
+                    :error-messages="gear.errorMessage.value"></v-number-input>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-number-input v-model="formData.circumference" label="Umfang"></v-number-input>
+                  <v-number-input v-model="circumference.value.value" label="Umfang"
+                    :error-messages="circumference.errorMessage.value"></v-number-input>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-number-input v-model="formData.width" label="Breite"></v-number-input>
+                  <v-number-input v-model="width.value.value" label="Breite"
+                    :error-messages="width.errorMessage.value"></v-number-input>
                 </v-col>
                 <v-col cols="12" sm="6">
                   <v-number-input v-model="formData.kmStand" label="Km Stand"></v-number-input>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-select v-model="formData.warehouse" label="Lager" :items="warehouseOptions" outlined dense />
+                  <v-select v-model="warehouse.value.value" label="Lager" :items="warehouseOptions" outlined dense
+                    :error-messages="warehouse.errorMessage.value" />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-number-input v-model="formData.kmStand" label="Lager Platz"></v-number-input>
+                  <v-number-input v-model="slot.value.value" label="Lager Platz"
+                    :error-messages="slot.errorMessage.value"></v-number-input>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-select v-model="formData.condition" label="Sleeve-Zustand" :items="conditionOptions"
-                    item-title="label" item-value="value" outlined dense />
+                  <v-select v-model="condition.value.value" label="Sleeve-Zustand" :items="conditionOptions"
+                    item-title="label" item-value="value" outlined dense
+                    :error-messages="condition.errorMessage.value" />
                 </v-col>
                 <v-col cols="12">
                   <v-textarea v-model="formData.notes" label="Anmerkingen" rows="3" outlined dense />
