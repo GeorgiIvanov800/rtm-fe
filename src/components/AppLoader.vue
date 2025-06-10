5c4567dfb5a5cd3ee36f17564910799aec6fe156
georgi <Georgi_Ivanov92@pm.me>
2025-06-10 18:56:56 +0200

    created pinia store for loading component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const overlay = ref(false);
+
+</script>
+
+
+<template>
+  <div class="text-center">
+    <v-overlay :model-value="overlay" class="align-center justify-center">
+      <v-progress-circular color="primary" size="64" indeterminate></v-progress-circular>
+    </v-overlay>
+  </div>
+</template>
